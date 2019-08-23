db91d05196634ff65fb88f9db7ae7a407797bbbe
suheyl z <suheylz@hotmail.com>
2019-08-23 16:06:55 +0500

    hellow world

---
+++ src/main/scala/Solution.scala
@@ -0,0 +1,17 @@
+object Solution {
+
+  def queensAttack(n: Int, k: Int, r_q: Int, c_q: Int, obstacles: Array[Array[Int]]): Int = {
+
+    val locations = obstacles.map(x=> (x.head, x.tail.head)).toList
+
+
+    
+
+
+
+
+    0
+  }
+
+
+}
